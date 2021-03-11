8535411b1b0e77d538ae1ea022bc9dbb0efe1206
robotic7o7 <scientificrohan@gmail.com>
2021-03-11 22:23:46 +0530

    latest commit

--- src/components/contact-section/contact-section.js
+++ src/components/contact-section/contact-section.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import './contact-section.css';
 
 function ContactSection() {
@@ -12,6 +13,13 @@ function ContactSection() {
                     <div className="content-container-sec">
                         <span className="contact-content">Have a question or need directions? We’re here to help you get to the right place.</span>
                     </div>
+
+                </div>
+                <div className="contact-button-container">
+                    <Link className="contact-button">Contact Us<svg className="contact-button-left-arrow" width="22" height="19" viewBox="0 0 22 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M11.9068 0.488281L20.9127 9.49399M20.9127 9.49399L11.9068 18.4999M20.9127 9.49399H0.495117" stroke="black" />
+                    </svg>
+                    </Link>
                 </div>
                 <div className="contact-section-title-container">
                     <span className="contact-section-title">Get in Touch</span>
--- src/components/vision-section/vision-section.js
+++ src/components/vision-section/vision-section.js
@@ -49,8 +49,21 @@ function VisionSection() {
                         <span className="vision-points" onMouseEnter={e => { e.preventDefault(); display4(e) }} onMouseLeave={e => { e.preventDefault(); hide4(e) }}>Perseverance with evolution on a continuous basis</span>
                     </div>
                 </div>
+
+                <div className="vision-container-mob">
+                    <div className="vision-box">
+                        <span className="vision-heading">WE BELIEVE IN</span>
+                        <span className="vision-points" onClick={e => { e.preventDefault(); display1(e) }} >Co-Ownership as the highest form of loyalty</span>
+                        <span className="vision-points" onClick={e => { e.preventDefault(); display2(e) }} >Upholding Highest Ethical Standards</span>
+                        <span className="vision-points" onClick={e => { e.preventDefault(); display3(e) }} >Flexibility in combination with Dynamism</span>
+                        <span className="vision-points" onClick={e => { e.preventDefault(); display4(e) }} >Perseverance with evolution on a continuous basis</span>
+                    </div>
+                </div>
                 <div className="hover-card1" id="hover-card1">
                     <div className="hover-card-container">
+                        <div className="close-icon-container">
+                            <img src="/close-icon.svg" className="close-icon" onClick={e => { e.preventDefault(); hide2(e) }} />
+                        </div>
                         <div className="hover-card">
                             <img src="/ethical.png" className="hover-card-image"></img>
                             <span className="hover-card-heading">Highest Ethical Standards</span>
@@ -63,10 +76,14 @@ function VisionSection() {
                 </div>
 
                 <div className="hover-card2" id="hover-card2">
+
                     <div className="pointer-container">
                         <div class="arrow-left1"></div>
                     </div>
                     <div className="hover-card-container">
+                        <div className="close-icon-container">
+                            <img src="/close-icon.svg" className="close-icon" onClick={e => { e.preventDefault(); hide1(e) }} />
+                        </div>
                         <div className="hover-card">
                             <img src="/coownership.png" className="hover-card-image"></img>
                             <span className="hover-card-heading">Co-Ownership</span>
@@ -80,6 +97,9 @@ function VisionSection() {
                         <div class="arrow-left2"></div>
                     </div>
                     <div className="hover-card-container">
+                        <div className="close-icon-container">
+                            <img src="/close-icon.svg" className="close-icon" onClick={e => { e.preventDefault(); hide3(e) }} />
+                        </div>
                         <div className="hover-card">
                             <img src="/coownership.png" className="hover-card-image"></img>
                             <span className="hover-card-heading">Flexibility</span>
@@ -90,6 +110,9 @@ function VisionSection() {
 
                 <div className="hover-card4" id="hover-card4">
                     <div className="hover-card-container">
+                        <div className="close-icon-container">
+                            <img src="/close-icon.svg" className="close-icon" onClick={e => { e.preventDefault(); hide4(e) }} />
+                        </div>
                         <div className="hover-card">
                             <img src="/perseverence.png" className="hover-card-image"></img>
                             <span className="hover-card-heading">Perseverence</span>
